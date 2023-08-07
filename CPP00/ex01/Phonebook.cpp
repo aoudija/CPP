da70600d105559f8e27e2aa9677e6f0fa5bf1a91
Amine Oudija <amineoudija02@yahoo.com>
2023-08-07 08:34:17 +0100

    commit

--- CPP00/ex01/Phonebook.cpp
+++ CPP00/ex01/Phonebook.cpp
@@ -43,11 +43,41 @@ void	columns()
 //getter
 void	PhoneBook::show_phonebook(){
 	columns();
-	for (int i = 0; i < this->num; i++) {
+	std::string line;
+	int i;
+	for (i = 0; i < this->num; i++) {
 		std::cout << (i + 1) << "         |";
 		printer(this->c[i].gf_name()); std::cout << + "|";
 		printer(this->c[i].gl_name()); std::cout << + "|";
 		printer(this->c[i].gnick_name()); std::cout << + "|";
 		std::cout << "\n";
    	}
+	while (1){
+		std::cout << "enter index : ";
+		std::getline(std::cin, line);
+		if (std::cin.eof())
+			break ;
+		if (line.length() != 1 || !isdigit(line[0])){
+			std::cout << "wrong index\n";}
+		else{
+			int n = std::stoi(line);
+			if (n <= 7 && n >=0)
+			{
+				if (n < i)
+				{
+					std::cout << (n + 1) << "         |";
+					printer(this->c[n].gf_name()); std::cout << + "|";
+					printer(this->c[n].gl_name()); std::cout << + "|";
+					printer(this->c[n].gnick_name()); std::cout << + "|";
+					std::cout << "\n";
+					break ;
+				}
+				else
+					std::cout << "error occured!\n";break ;
+			}
+			else
+				std::cout << "not in range , try (0 -> 7)\n";
+		}
+	}
+		std::cout << std::setw(2) << "abrakadabra\n";
 }
