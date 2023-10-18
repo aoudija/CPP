8d4a936ad667fa4be53b8487f7293fac37125826
Amine Oudija <amineoudija02@yahoo.com>
2023-10-18 15:26:19 +0100

    commit

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -17,15 +17,6 @@ void	Harl::error(void){
 }
 
 void	Harl::complain(std::string level){
-	std::string tab[] = {"debug", "info", "warning", "error"};
-	void	(Harl::*pf[])() = {&Harl::debug, &Harl::info, &Harl::warning
-		, &Harl::error};
-	int i;
-	for (i = 0;i < 4;i++){
-		if (tab[i] == level){
-			(this->*pf[i])();
-			break ;
-		}
-	}
+	
 }
 
