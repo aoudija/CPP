f37a88e2c81d67aa170057a511badc41a7ea314d
Amine Oudija <amineoudija02@yahoo.com>
2023-10-18 17:03:52 +0100

    commit

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -1,11 +1,5 @@
 #include "Zombie.hpp"
 
-void	randomChump(std::string name){
-	Zombie* Z = newZombie(name);
-	Z->announce();
-	delete Z;
-}
-
 Zombie*	zombieHorde(int N, std::string name){
 	Zombie*	Z = new Zombie();
 	for (int i = 0; i < N; i++){
